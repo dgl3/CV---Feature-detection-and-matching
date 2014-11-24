fffdc57256499a8177029cde14d33f675f883c91
dgl3 <danigibert24@gmail.com>
2014-11-24 11:01:57 +0100

    Roof image with different peak tresh

--- feature_detection.m
+++ feature_detection.m
@@ -1,18 +1,32 @@
 %Roof image
-I = vl_impattern('roofs1');
-I = single(rgb2gray(I));
-figure, imshow(I);
-[f,d] = vl_sift(I);
+Iroofs = vl_impattern('roofs1');
+Iroofs = single(rgb2gray(Iroofs));
+figure, imshow(Iroofs);
+[f,d] = vl_sift(Iroofs);
 
 %Show the descriptors
-figure, show_keypoints(I,f);
+figure, show_keypoints(Iroofs,f);
 
-
+%{
 %River image
-I = vl_impattern('river1');
-I = single(rgb2gray(I));
-figure, imshow(I);
-[f,d] = vl_sift(I);
+Iriver = vl_impattern('river1');
+Iriver = single(rgb2gray(Iriver));
+figure, imshow(Iriver);
+[f,d] = vl_sift(Iriver);
 
 %Show the descriptors
-figure, show_keypoints(I,f);
+figure, show_keypoints(Iriver,f);
+
+%}
+%Testing the SIFT detector
+[f,d] = vl_sift(Iroofs,'PeakThresh', 0.01);
+figure, show_keypoints(Iroofs,f);
+
+[f,d] = vl_sift(Iroofs,'PeakThresh', 0.03);
+figure, show_keypoints(Iroofs,f);
+
+[f,d] = vl_sift(Iroofs,'PeakThresh', 0.05);
+figure, show_keypoints(Iroofs,f);
+
+[f,d] = vl_sift(Iroofs,'PeakThresh', 0.08);
+figure, show_keypoints(Iroofs,f);
